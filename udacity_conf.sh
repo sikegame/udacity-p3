f823febe2696c30b997ce692e8b563aab464088a
JJ <jj@digitalnomad.jp>
2015-12-16 07:27:42 +0000

    Fix database error

--- udacity_conf.sh
+++ udacity_conf.sh
@@ -3,4 +3,4 @@
 sudo apt-get update -y
 sudo apt-get upgrade -y
 
-sudo apt-get install -y apache2 libapache2-mod-wsgi postgresql python-pip git
+sudo apt-get install -y apache2 libapache2-mod-wsgi postgresql python-pip git python-psycopg2
